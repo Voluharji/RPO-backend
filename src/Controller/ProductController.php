1afddf1327d8f3227893b960b5bdd9efc279dade
NejcSorgo <nejc.sorgo@gmail.com>
2024-12-01 16:51:15 +0100

    added basic product api

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,28 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/api/getProduct', name: 'get_product', methods: ['GET'])] // en izdelek fetcha po id
+    #[Route('/api/get_product', name: 'get_product', methods: ['GET'])] // en izdelek fetcha po id
     public function getProduct( EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $request = Request::createFromGlobals();
+        $productRepository = $entityManager->getRepository(Product::class);
+        if ($request->get('id') == null) {
+            return new JsonResponse(["No product id provided."],400);
+        }
+        $product = $productRepository->getProductById($request->get('id'));
+        if ($product == null) {
+            return new JsonResponse(["Product does not exist."],404);
+        }
+        return $this->json(
+           $product
+        ,200);
     }
     #[Route('/api/getProducts', name: 'get_products', methods: ['GET'])] // po 15 izdelkov fetcha
     public function getProducts( EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $request = Request::createFromGlobals();
+        $productRepository = $entityManager->getRepository(Product::class);
+        if ($request->get('id') == null) {
+            return new JsonResponse(["No product id provided."],400);
+        }
+        $products = $productRepository->getProducts($request->get('offset'));
+        if ($products == null) {
+            return new JsonResponse(["Product does not exist."],404);
+        }
+        return $this->json(
+            $products
+        ,200);
+
     }
 
 }
